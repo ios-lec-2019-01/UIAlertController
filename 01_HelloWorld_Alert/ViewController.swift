7e42232b3250ba1a2220bfdc04e2f3e1c6666a63
Jonghyun Kim <jhkim@dit.ac.kr>
2018-04-30 10:38:44 +0900

    mid exam 01

--- 01_HelloWorld_Alert/ViewController.swift
+++ 01_HelloWorld_Alert/ViewController.swift
@@ -10,16 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var outLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func buttonPressed(_ sender: Any) {
+        let alertController = UIAlertController(title: "카메라", message: "사진을 촬영합니다", preferredStyle: .alert)
+        
+        let camAction = UIAlertAction(title: "카메라 열기", style: .default) {
+            (action: UIAlertAction) -> Void in
+            //self.view.backgroundColor = UIColor.blue
+            self.outLabel.text = "카메라 열기 실행됨"
+        }
+        
+        let libAction = UIAlertAction(title: "사진 폴더 열기", style: .default) {
+            (action: UIAlertAction) -> Void in
+            //self.view.backgroundColor = UIColor.yellow
+            self.outLabel.text = "사진 폴더 열기 실행됨"
+        }
+        
+        let cancelAction = UIAlertAction(title: "취소", style: .cancel) {
+            (action: UIAlertAction) -> Void in
+            self.view.backgroundColor = UIColor.white
+            self.outLabel.text = "취소 실행됨"
+        }
+        
+        alertController.addAction(camAction)
+        alertController.addAction(libAction)
+        alertController.addAction(cancelAction)
+        
+        present(alertController, animated: true, completion: nil)
     }
-
-
 }
 
